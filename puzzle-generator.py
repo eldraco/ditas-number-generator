230c1d0854ce4a0a983c1b88e5f4112a687ce97f
Sebastian Garcia <eldraco@gmail.com>
2020-05-23 14:46:06 +0200

    Version working

--- puzzle-generator.py
+++ puzzle-generator.py
@@ -1,24 +1,35 @@
 #!/usr/bin/env python
+# Python implementation of the game sent by dita of finding which is the next sequence in
+# 6
+# 61
+# 6111
+# 6113
+# 611231
+# After finding the sequence, this is the generator i did
+# Author: Sebastian Garcia, eldraco@gmail.com
 
-line_to_print = '661'
+iterations = 20
+# Start with this line
+line_to_print = '6'
+# The first line should be printed manually
 print(f'{line_to_print}')
 
-while line_to_print:
-    # Get each number in the line
+while line_to_print and iterations:
     line = line_to_print
     line_to_print = ''
-
+    # The index of the 'main' numbers that do not repeat, the numbers to search for
     number_index = 0
-    # i starts in 1 so we dont count the 'number' twice. counter is 1 too
-    internal_index = 1
+    # Internal index while searching for repeated numbers
+    internal_index = 0
     while number_index < len(line):
+        # Get the current main number
         number = line[number_index]
         # Add the next number to print
         line_to_print += number
-        # The number appears at least once
-        counter = 1
+        # The number of times the main number appears
+        counter = 0
         while internal_index < len(line):
-            # Since i starts in 1 instead of 0, the comparision is against line[i] instead of line[i + 1]
+            # Is the main number repeated once more?
             if line[internal_index] == number:
                 counter += 1
                 internal_index += 1
@@ -27,14 +38,8 @@ while line_to_print:
                 break
         # The current number we need to print the count
         line_to_print += str(counter)
-        # We need now to continue with the rest of the numbers in this line
-        # if i > len(line) there are no more numbers to process, if i <= len(line) we need to continue
-        if internal_index <= len(line):
-            number_index = internal_index - 1
-
-    line = line_to_print
-    print(f'{line}')
-
-
-
+        # We need now to continue with the rest of the main numbers in this line, if there are any
+        number_index = internal_index
+    print(f'{line_to_print}')
+    iterations -= 1
 
